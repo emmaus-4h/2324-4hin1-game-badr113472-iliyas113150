7e04950b574ab43af588daccfdffde6777329b5c
badri8 <146429825+badri8@users.noreply.github.com>
2024-05-24 10:17:57 +0000

    game over shcermm

--- script.js
+++ script.js
@@ -17,7 +17,7 @@
 /* ********************************************* */
 const SPELEN = 1;
 const GAMEOVER = 2;
-const UITLEG = 8;
+const UITLEG = 3;
 var spelStatus = SPELEN;
 
 const KEY_w = 87;
@@ -45,34 +45,34 @@ var health = 100;
  */
 var beweegAlles = function() {
   // speler
-if (keyIsDown(KEY_a)) {
-  spelerX = spelerX -1;
-}  
+  if (keyIsDown(KEY_a)) {
+    spelerX = spelerX - 1;
+  }
   if (keyIsDown(KEY_d)) {
-    spelerX = spelerX +1;
+    spelerX = spelerX + 1;
   }
 
-if (keyIsDown(KEY_w)) {
-  spelerY = spelerY -1
-}
+  if (keyIsDown(KEY_w)) {
+    spelerY = spelerY - 1
+  }
   if (keyIsDown(KEY_s)) {
-    spelerY = spelerY +1
+    spelerY = spelerY + 1
   }
   // vijand
-if (keyIsDown(KEY_LEFT)) {
-  vijandX= vijandX -1;
-}
+  if (keyIsDown(KEY_LEFT)) {
+    vijandX = vijandX - 1;
+  }
 
   if (keyIsDown(KEY_UP)) {
-    vijandY = vijandY -1;
+    vijandY = vijandY - 1;
   }
 
   if (keyIsDown(KEY_DOWN)) {
-    vijandY = vijandY +1
+    vijandY = vijandY + 1
   }
 
   if (keyIsDown(KEY_RIGHT)) {
-    vijandX = vijandX +1;
+    vijandX = vijandX + 1;
   }
   // kogel
 };
@@ -82,33 +82,38 @@ if (keyIsDown(KEY_LEFT)) {
  * Verwijdert neergeschoten dingen
  * Updatet globale variabelen punten en health
  */
-var verwerkBotsing = function  () {
+var verwerkBotsing = function() {
   // botsing speler tegen vijand
-if (spelerX - vijandX <50 &&
-    spelerX - vijandX > -50 &&
-    spelerY - vijandY <50 &&
-    spelerY - vijandY > -50) {
-  console.log("Botsing");
-    }
-  
+
+
   // botsing kogel tegen vijand
 
   // update punten en health
 
 };
+var checkGameOver = function() {
+  if (spelerX - vijandX < 50 &&
+    spelerX - vijandX > -50 &&
+    spelerY - vijandY < 50 &&
+    spelerY - vijandY > -50) {
+    console.log("Botsing");
 
+    return true;
+  }
+  return false;
+}
 /**
  * Tekent spelscherm
  */
-var tekenAlles = function() {  
+var tekenAlles = function() {
   // achtergrond
-fill ("green");
-  rect(0,0,1280,720);
-  
+  fill("green");
+  rect(0, 0, 1280, 720);
+
   // vijand
-  fill ("red");
-  rect (vijandX - 25, vijandY - 25, 50, 50)
-  fill ("blue");
+  fill("red");
+  rect(vijandX - 25, vijandY - 25, 50, 50)
+  fill("blue");
   ellipse(vijandX, vijandY, 10, 10);
   // kogel
 
@@ -149,20 +154,22 @@ function draw() {
     beweegAlles();
     verwerkBotsing();
     tekenAlles();
-    if (health <= 0) {
+    if (checkGameOver()) {
       spelStatus = GAMEOVER;
+      console.log("game over")
     }
+    console.log("spelen");
   }
-   if (spelStatus === GAMEOVER) {
-     // teken gameover scherm
-     text("game over", 200, 200);
-     
+  if (spelStatus === GAMEOVER) {
+    // teken gameover scherm
+    console.log("game over");
+    textSize(60);
+    fill("white");
+    text("Game Over", 100, 100);
+
   }
   if (spelStatus === UITLEG) {
     // teken uitleg scherm
-     text("druk op spatie", 200, 300);
-    if (keyIsDown(32)) {
-      spelStatus = SPELEN;
-    }
+    console.log("uitleg");
   }
 }
